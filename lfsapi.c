c3cf7656e1b6bdfb6af3c0ec741a5cf25a38e739
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2024-09-07 09:15:06 +0800

    cleanup

--- lfsapi.c
+++ lfsapi.c
@@ -16,16 +16,7 @@ const struct lfs_config *new_lfs_config(void *read, void *prog, void *erase, voi
         .erase = erase,
         .sync = sync,
 
-        // block device configuration
-		/*
-        .read_size = 256,
-        .prog_size = 32,
-        .block_size = block_size,
-        .block_count = block_count,
-        .cache_size = 256 * 4,
-        .lookahead_size = 32,
-        .block_cycles = 100,
-		*/
+        // block device configuration mklittlefs compat
 		// derived from https://github.com/earlephilhower/mklittlefs/blob/master/main.cpp
         .read_size = 64,
         .prog_size = 64,
@@ -34,7 +25,7 @@ const struct lfs_config *new_lfs_config(void *read, void *prog, void *erase, voi
         .cache_size = 64,
         .lookahead_size = 64,
         .block_cycles = 16,
-		//.read_buffer = nullptr,
+		//.read_buffer = nullptr, 
 		//.prog_buffer = nullptr,
 		//.lookahead_buffer = nullptr,
 		.name_max = 0,
@@ -58,16 +49,14 @@ void lfs_write_file(lfs_t *lfs, char *name, void *data, size_t size) {
 	
 }
 
-void lfs_write_dir(lfs_t *lfs, char *name, char *path){
+void lfs_write_dir(lfs_t *lfs, char *name){
 	lfs_dir_t dir;
 	struct stat sbuf;	
 	lfs_dir_open(lfs, &dir, name);
 	lfs_mkdir(lfs, name);
-	if (!stat(path, &sbuf)) {
-		uint32_t ftime = sbuf.st_mtime;
-		lfs_setattr(lfs, path, 't', (const void *)&ftime, sizeof(ftime));
-		lfs_setattr(lfs, path, 'c', (const void *)&ftime, sizeof(ftime));
-	}
+	uint32_t ftime = sbuf.st_mtime;
+	lfs_setattr(lfs, name, 't', (const void *)&ftime, sizeof(ftime));
+	lfs_setattr(lfs, name, 'c', (const void *)&ftime, sizeof(ftime));
 	lfs_dir_close(lfs, &dir);
 }
 
